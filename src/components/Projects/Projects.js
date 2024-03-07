4428ce6168075ae60c63f689f344457776d81110
aasif1297 <aasif1297@gmail.com>
2024-03-07 10:59:14 +0100

    added  mg_customer app in projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import comingSoon from "../../Assets/Projects/coming_soon.png";
+import mygharCustomer from "../../Assets/Projects/myghar_customer.png";
 
 
 function Projects() {
@@ -19,12 +19,12 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={comingSoon}
+              imgPath={mygharCustomer}
               isBlog={false}
-              title="Under Development"
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis ullamcorper justo, ac molestie felis maximus non. Nam suscipit tortor convallis dui fringilla condimentum. Suspendisse eget feugiat libero. Cras at commodo ligula, porta facilisis dolor. Aliquam pulvinar dignissim odio, et iaculis eros lobortis at. Ut commodo viverra libero et maximus. Nulla arcu est, consectetur at ex a, lacinia mollis nibh. Ut tincidunt vehicula molestie. Ut id quam et nisi malesuada aliquam. Vivamus posuere venenatis ante ut efficitur. Proin vehicula mattis tellus eu auctor. Integer gravida, lectus eu luctus elementum, lacus lectus euismod ex, nec luctus libero magna id ipsum. Morbi pretium euismod egestas. Maecenas elit est, fringilla a ultrices a, porta non erat."
+              title="MyGhar Customer App"
+              description="Hassle Free Living All inclusive living spaces designed for young professionals & travelers."
               ghLink=""
-              demoLink=""
+              demoLink="https://play.google.com/store/apps/details?id=com.mygharliving.my_ghar_customer"
             />
           </Col>
 
