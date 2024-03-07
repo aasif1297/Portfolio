cb1f5c9b861bb72e46910b4134c4dd4c7c86f7ea
aasif1297 <aasif1297@gmail.com>
2024-03-07 11:03:06 +0100

    added eq_client in projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import mygharCustomer from "../../Assets/Projects/myghar_customer.png";
+import eqClient from "../../Assets/Projects/eq_client.png";
 
 
 function Projects() {
@@ -25,6 +26,34 @@ function Projects() {
               description="Hassle Free Living All inclusive living spaces designed for young professionals & travelers."
               ghLink=""
               demoLink="https://play.google.com/store/apps/details?id=com.mygharliving.my_ghar_customer"
+            />
+            <ProjectCard
+              imgPath={eqClient}
+              isBlog={false}
+              title="Equigate Customer App"
+              description="The all-in-one app for equine professionals.
+
+              Equigate is an all-in-one app for all equine professionals. With effortless bookings, secure payments, direct messaging, instant and dependable reports plus many other features. Equigate provides reliability, structure and efficiency allowing you to focus on what you do best.
+              
+              There are profiles for everyone with tailored areas specific to your profession:
+              
+              •Equine and canine body workers and therapists
+              •Farriers
+              •Dentists
+              •Saddle fitters
+              
+              The future of your business, in your pocket
+              
+              Saving you and your clients time, Equigate takes care of all administrative angles, allowing you to focus on the practice you know and love.
+              Less time on admin, so you can spend more time doing what you’re great at
+              
+              
+              Records, communication, payments. Everything lives inside the app
+              
+              The all-in-one nature of Equigate allows you to maintain complete control of your schedule.
+              Centralising your messaging and appointment system, simple record management, and visible in app payment management."
+              ghLink=""
+              demoLink="https://play.google.com/store/apps/details?id=com.equigate.client"
             />
           </Col>
 
